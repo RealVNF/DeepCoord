737376f6ce284be0c0d6b96ce4ffe42f622fecd9
Stefan Schneider <stefan.schneider@upb.de>
2021-12-01 13:47:58 +0100

    add action for pypi publish

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
+import os
 from setuptools import setup, find_packages
+
+
+# read the contents of your README file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 requirements = [
     'click==7.0',
@@ -22,6 +30,8 @@ setup(
     name='deepcoord',
     version='1.1.1',
     description='DeepCoord: Self-Learning Network and Service Coordination Using Deep Reinforcement Learning',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/RealVNF/DeepCoord',
     author='RealVNF',
     package_dir={'': 'src'},
